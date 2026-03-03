defd858905af9fbac66f26853dfb143d39a19e02
Claudio Piazzai <claudio@MacBookPro.homenet.telecomitalia.it>
2026-03-03 16:28:41 +0100

    Configure GitHub Pages Deployment

--- dataroom-app/src/App.tsx
+++ dataroom-app/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import GlobalLayout from './components/layout/GlobalLayout';
 import { ThemeProvider } from './components/ThemeProvider';
 import InvestmentThesisPage from './pages/InvestmentThesisPage';
