8302fda516512b19473182a29080009bb35480e6
Claudio Piazzai <claudio@192.168.1.54>
2026-03-04 09:50:56 +0100

    Updates

---
+++ assets/stateDiagram-v2-4FDKWEC3-vMC4kW0B.js
@@ -0,0 +1,1 @@
+import{s as t,b as r,a,S as s}from"./chunk-DI55MBZ5-B5TICXbB.js";import{_ as i}from"./index-BYcIsHWZ.js";import"./chunk-55IACEB6-BFkIB_tl.js";import"./chunk-QN33PNHL-DL2CJiMP.js";var l={parser:a,get db(){return new s(2)},renderer:r,styles:t,init:i(e=>{e.state||(e.state={}),e.state.arrowMarkerAbsolute=e.arrowMarkerAbsolute},"init")};export{l as diagram};
